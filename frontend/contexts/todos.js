ba1b7d16f5ad45a6e5786209ddbf0689fe7053fd
Ladrillo <gabriel@cabrejas.net>
2022-05-19 18:43:28 +0200

    committing to "lecture"

--- frontend/contexts/todos.js
+++ frontend/contexts/todos.js
@@ -1,7 +1,9 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 export const TodosContext = createContext()
 
 export default function TodosProvider(props) {
-  const []
+  const [todos, setTodos] = useState([])
+
+  
 }
