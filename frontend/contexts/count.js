72dd600a2e97cb7161b7b460fb1e0213a5b4f4ba
Ladrillo <gabriel@cabrejas.net>
2022-05-19 18:11:35 +0200

    committing to "lecture"

--- frontend/contexts/count.js
+++ frontend/contexts/count.js
@@ -5,8 +5,10 @@ export const CountContext = createContext() // will be used by consumers of cont
 
 export default function CountProvider(props) {
   // create some state and callbacks
+  // components will be able to use count, inc, dec
   const [count, setCount] = useState(0)
   const inc = () => setCount(count + 1)
-  const dev = () => setCount(count - 1)
+  const dec = () => setCount(count - 1)
 
+  
 }
