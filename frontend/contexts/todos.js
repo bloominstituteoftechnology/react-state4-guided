a7e906264360e13be9e19f0ed445077ec753ce8a
Ladrillo <gabriel@cabrejas.net>
2022-05-19 18:44:30 +0200

    committing to "lecture"

--- frontend/contexts/todos.js
+++ frontend/contexts/todos.js
@@ -1,9 +1,16 @@
 import React, { createContext, useState } from 'react'
+import axios from 'axios'
 
 export const TodosContext = createContext()
 
 export default function TodosProvider(props) {
   const [todos, setTodos] = useState([])
 
-  
+  function fetchTodos() {
+    axios.get('http://localhost:9000/api/todos')
+      .then(() => {
+
+      })
+      .catch(())
+  }
 }
