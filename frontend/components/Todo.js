e4bb6dd0677d945c03cbb95d521878c75152fc74
Ladrillo <gabriel@cabrejas.net>
2022-05-19 18:28:56 +0200

    committing to "lecture"

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -4,9 +4,10 @@ import { CountContext } from '../contexts/count'
 
 export default function Todo(props) {
   const { todo, toggleStatus } = props
+  const { count } = useContext(CountContext)
   return (
     <div onClick={toggleStatus(todo.id)} className="todo">
-      {todo.name}{todo.completed ? ' ✔️' : ''}
+      {todo.name} {count} times {todo.completed ? ' ✔️' : ''}
     </div>
   )
 }
