30ea8362ba38968b4779702f42d90e60e6807e04
Ladrillo <gabriel@cabrejas.net>
2022-05-19 18:12:06 +0200

    committing to "lecture"

--- frontend/contexts/count.js
+++ frontend/contexts/count.js
@@ -10,5 +10,9 @@ export default function CountProvider(props) {
   const inc = () => setCount(count + 1)
   const dec = () => setCount(count - 1)
 
-  
+  return (
+    <CountContext.Provider>
+      
+    </CountContext.Provider>
+  )
 }
