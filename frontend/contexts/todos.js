74f9c558f9a687d28bf73894705973f9918b23c8
Ladrillo <gabriel@cabrejas.net>
2022-08-18 18:43:57 +0200

    committing to "lecture"

--- frontend/contexts/todos.js
+++ frontend/contexts/todos.js
@@ -3,12 +3,14 @@ import axios from 'axios'
 
 export const TodosContext = createContext()
 
+const url = 'http://localhost:9000/'
+
 export default function TodosProvider(props) {
   const [todos, setTodos] = useState([])
 
   async function fetchTodos() {
     try {
-      
+      const res = await axios.get(url)
     } catch (err) {
 
     }
